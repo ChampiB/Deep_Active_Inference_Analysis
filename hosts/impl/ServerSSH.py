6f0a34f4f2f28ac4135b98273ac818c32fd5834f
ChampiB <tmac3@kent.ac.uk>
2022-09-25 16:46:33 +0100

    update the agents and environments

--- hosts/impl/ServerSSH.py
+++ hosts/impl/ServerSSH.py
@@ -78,13 +78,11 @@ class ServerSSH(HostInterface):
         Execute a command and close all channel returned by the command call
         :param client: the client that must be used to run the command
         :param command: the command
-        :param:
         """
         stdin, stdout, stderr = client.exec_command(command)
         print(command)
         print(stdout.readlines())
         print(stderr.readlines())
-        status
         stdin.close()
         stdout.close()
         stderr.close()
