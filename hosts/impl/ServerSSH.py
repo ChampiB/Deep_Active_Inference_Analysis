e5002265f9c30e6b906dd932d860578b81ba3b71
ChampiB <tmac3@kent.ac.uk>
2022-09-26 13:45:05 +0100

    update the agents and environments

--- hosts/impl/ServerSSH.py
+++ hosts/impl/ServerSSH.py
@@ -116,6 +116,7 @@ class ServerSSH(HostInterface):
             if job["status"] != "crashed":
                 return
 
+        return
         # Start the job
         self.setup_ssh_server(client)
         project_dir = self.repository_path + f"data/projects/{project_name}/"
